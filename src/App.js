4427f6886cc2d0d26b8f899b435cc27d469e4e8f
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-19 17:00:00 +0200

    Weather API:HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import clear from "./img/weather-icons/clear.svg";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Abdel Rahman"
+      name: "Abdel Rahman&taleb khaddour"
     };
   }
 
@@ -22,19 +24,20 @@ class App extends Component {
         </nav>
         <main className="hero">
         <figure>
-            <img src="https://placekitten.com/200/300" className="hero-image" />
-            <figcaption>overcast clouds</figcaption>
+            <img src={mostlycloudy} className="hero-image" />
+            <figcaption><b>overcast clouds</b></figcaption>
           </figure>
          <div class="main-text">
             <p>
               <div class="temp1">
-              <b>Temperature</b>
-              <span className="temperatue1">10&deg; to 11&deg; C</span>
+              <p className="litel"><b>Temperature</b>
+              <span className="temperatue1 "> &nbsp;&nbsp;&nbsp;10&deg; to 11&deg; C </span></p>
 
               </div>
+              <p class="pourcentage-1">
               <b>Humidity</b>
-              <span className="pourcentage-1">78%</span> <b>Pressure</b>
-              <span className="pourcentage-2">1008.48</span>
+              <span className="pourcentage-1">&nbsp;&nbsp;78%&nbsp;&nbsp;</span> <b>Pressure</b>
+              <span className="pourcentage-2">&nbsp;&nbsp;1008.48&nbsp;&nbsp;</span></p>
             </p>
           </div>
         </main>
@@ -42,43 +45,43 @@ class App extends Component {
         <div className="hourly-weather">
           <article>
               <time>03:00</time>
-              <img src="https://placekitten.com/g/200/300" />
+              <img src={mostlycloudy} />
               <span>8&deg;C</span>
           </article>
 
           <article>
               <time>06:00</time>
-              <img src="https://placekitten.com/g/200/300" />
+              <img src={mostlycloudy} />
               <span>9&deg;C</span>
           </article>
 
           <article>
               <time>09:00</time>
-              <img src="https://placekitten.com/g/200/300" />
+              <img src={clear} />
               <span>14&deg;C</span>
           </article>
 
           <article>
               <time>12:00</time>
-              <img src="https://placekitten.com/g/200/300" />
+              <img src={clear}  />
               <span>17&deg;C</span>
           </article>
 
           <article>
               <time>15:00</time>
-              <img src="https://placekitten.com/g/200/300" />
+              <img src={clear}  />
               <span>16&deg;C</span>
           </article>
 
           <article>
               <time>21:00</time>
-              <img src="https://placekitten.com/g/200/300" />
+              <img src={clear} />
               <span>8&deg;C</span>
           </article>
 
           <article>
               <time>03:00</time>
-              <img src="https://placekitten.com/g/200/300"/>
+              <img src={mostlycloudy}/>
               <span>13&deg;C</span>
           </article>
         </div>
